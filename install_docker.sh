372676f87e8ea775646c9b7d427e15c0eba33545
Rodney Cobb <roacobb@gmail.com>
2015-09-15 10:38:27 -0400

    Refactored

--- install_docker.sh
+++ install_docker.sh
@@ -3,5 +3,6 @@
 sudo apt-get -y update
 sudo apt-get -y install curl git nano wget
 sudo curl -sSL https://get.docker.com/ | sh
-sudo useradd -m -p containers -s /bin/bash ajplusdev
+sudo adduser --quiet --disabled-password -shell /bin/bash --home /home/newuser --gecos ajplusdev newuser
+sudo echo ajplusdev:containers | chpasswd
 sudo echo 'ajplusdev ALL(ALL:ALL) ALL' >> /etc/sudoers
