f36a37d4b72f6685963f68b94e4d2c57ea34c2df
Rodney Cobb <roacobb@gmail.com>
2015-09-15 10:20:43 -0400

    Refactored

--- install_docker.sh
+++ install_docker.sh
@@ -3,4 +3,5 @@
 sudo apt-get -y update
 sudo apt-get -y install curl git nano wget
 sudo curl -sSL https://get.docker.com/ | sh
-sudo useradd -m -p docker -s /bin/bash ajplusdev
+sudo echo 'ajplusdev ALL(ALL:ALL) ALL' >> /etc/sudoers
+sudo useradd -m -p containers -s /bin/bash ajplusdev
