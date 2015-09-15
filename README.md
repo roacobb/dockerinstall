de6d7935ff0a8af53007d9ed2e9dba598d99cfb5
Rodney Cobb <roacobb@gmail.com>
2015-09-15 09:58:35 -0400

    Refactored and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Docker Install script for Ubuntu 14 ##
+
+Install with `sudo wget https://rawgit.com/roacobb/dockerinstall/master/install_docker.sh`
+Run shell script `sudo sh install_docker.sh`
--- install_docker.sh
+++ install_docker.sh
@@ -3,4 +3,4 @@
 sudo apt-get -y update
 sudo apt-get -y install curl git nano wget
 sudo curl -sSL https://get.docker.com/ | sh
-sudo useradd -m -p <docker> -s /bin/bash <ajplusdev>
+sudo useradd -m -p docker -s /bin/bash ajplusdev
